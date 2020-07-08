51f6b9cc5c0d6ab3ff74e5e32e6408aa8ec86c0f
Danyaka <48256141+Danyaka@users.noreply.github.com>
2020-07-08 10:47:55 +0300

    Add files via upload

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            return ModelState.IsValid ? View("Thanks", guest) : View();
+        }
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите своё имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите Email.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите телефон")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите, примите ли вы участие в вечеринке")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
